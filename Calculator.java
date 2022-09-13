1b9a4a3bd99ea5a985ae70f8400e154073498bbc
Tomek Toczynski <tomek-toczynski@wp.pl>
2022-09-13 19:29:13 +0200

    Initial Commit

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(6, 10));
+        System.out.println(calculator.sub(6, 10));
+    }
+}
+
